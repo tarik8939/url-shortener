1b75b713f3e9f08f2714a117e484c7bde5d84b6c
Taras Horilchanyi <39026353+tarik8939@users.noreply.github.com>
2023-08-23 15:14:56 +0300

    Init DB

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/storage/sqlite"
 )
 
 const (
@@ -27,7 +29,14 @@ func main() {
 	)
 	log.Debug("debug messages are enabled")
 
-	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
 	// TODO: init router: chi, chi render
 	// TODO: run server:
 }
