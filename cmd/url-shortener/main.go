45c1b22ed3068bb699af6d90a02864b143093ec4
Taras Horilchanyi <39026353+tarik8939@users.noreply.github.com>
2023-12-06 17:39:11 +0200

    Added auth

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -53,9 +53,22 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.New(log, storage))
+	router.Route(
+		"/url", func(r chi.Router) {
+			r.Use(
+				middleware.BasicAuth(
+					"url-shortener", map[string]string{
+						cfg.HTTPServer.User: cfg.HTTPServer.Password,
+					},
+				),
+			)
+
+			r.Post("/", save.New(log, storage))
+			router.Delete("/{alias}", remove.New(log, storage))
+		},
+	)
+
 	router.Get("/{alias}", redirect.New(log, storage))
-	router.Delete("/{alias}", remove.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
